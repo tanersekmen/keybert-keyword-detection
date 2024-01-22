2127c15364752c2c678e25dcaa6a97cf598e3c84
tanersekmen <56790802+tanersekmen@users.noreply.github.com>
2024-01-22 17:05:07 +0300

    add constant values

---
+++ constant.py
@@ -0,0 +1,8 @@
+path = 'news_texts.csv'
+model_name = 'loodos/bert-base-turkish-uncased'
+n_keywords = 20
+ngram = 1
+stop_words = ['ise', 've', 'veya', 'belki', 'bazi', 'yaklasik',
+               'bir', 'cogu', 'de', 'daha', 'var', 'bircok',
+               'ancak', 'bu', 'şu', 'kasim', 'ayrica',
+               'yaptigi', 'eylul', 'oldukca', 'cok', 'onceki', 'sonraki' ]
